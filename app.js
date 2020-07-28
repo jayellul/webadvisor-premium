fe23c2e3f9797c094d3ce3cf24ea7e35f4113531
Josh Samuels <joshsamuels04@gmail.com>
2020-07-27 22:30:10 -0400

    Reads user emails and the courses they are subscribed to from DynamoDB

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
 // app.js
 
-const { exec } = require('child_process')
+const {
+  exec
+} = require('child_process')
 const puppeteer = require('puppeteer')
 const wait = require('waait')
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 const fs = require('fs')
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const _ = require('lodash')
-const { check, validationResult, matchedData } = require('express-validator');
+
+const {
+  check,
+  validationResult,
+  matchedData
+} = require('express-validator')
+
+const validator = require('validator')
+
+const AWS = require('aws-sdk')
 
 // Reads a .env file
-require('dotenv').config();
+require('dotenv').config()
 
 const app = express()
 
-// Time between each check in ms
-const msBetweenChecks = 0
+AWS.config.update({
+  region: 'us-east-1'
+})
+let ddb = new AWS.DynamoDB.DocumentClient()
+
+// Time between each check in ms (5 minutes)
+const msBetweenChecks = 300000
 
 // Webadvisor course variables
 const courseSemester = 'F20'
+
+// max 5 courses	
+const courses = ['CIS*3260', 'UNIV*2100', 'CIS*1250', 'NUTR*1010']
 
 // Headless?
 const headless = true
 
 const PORT = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const UNIX_DAY = 86400
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+/********************************
+ **** ROUTER CODE IS BROKEN *****
+ *** UNTIL IT WRITES TO DYNAMO **
+/********************************/
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/templates/inputForm.html')
 })
@@ -44,30 +71,36 @@ app.post('/subscribe', [
   // For details on the email normalization see the normalizeEmail at https://github.com/validatorjs/validator.js#sanitizers
   check('email').isEmail().withMessage('Invalid Email').bail().trim().normalizeEmail(),
 ], (req, res) => {
-  const formErrors = validationResult(req);
+  const formErrors = validationResult(req)
 
   if (!formErrors.isEmpty()) {
     console.error('formErrors', formErrors)
     return res.status(400).send(`formErrors -- ${JSON.stringify(formErrors)}`)
   }
-  
+
   // Ensures the app only uses data that has been validated
   const validatedData = matchedData(req)
 
   // start an instance of the service
-  start(validatedData.courses, validatedData.email)
+  updateCourseInfo(validatedData.courses, validatedData.email)
   return res.send('started a new process')
 })
 
-app.listen(PORT);
+// Runs the application with the constants defined above
+// TODO: Update to use all webadvisor data
+updateCourseInfo(courses)
 
+app.listen(PORT)
 
-async function start(courses, recipients) {
+async function updateCourseInfo(courses) {
   while (true) {
     try {
       const availableCourseInfo = await checkWebadvisor(courses)
+      
+      const courseKeys = Object.keys(availableCourseInfo)
+      const courseCodeEmailMap = await getEmailsCourseCodesFromDyanmo(courseKeys)
 
-      sendEmail(availableCourseInfo, recipients)
+      await sendEmail(availableCourseInfo, courseCodeEmailMap)
 
       await wait(msBetweenChecks)
     } catch (error) {
@@ -77,50 +110,60 @@ async function start(courses, recipients) {
 }
 
 async function checkWebadvisor(courses) {
-  const browser = await puppeteer.launch({ headless })
+  const browser = await puppeteer.launch({
+    headless
+  })
 
   const page = await browser.newPage()
 
   page.on('console', (msg) => console.log('PAGE LOG:', msg.text()))
 
   await page.goto('https://webadvisor.uoguelph.ca')
-  await page.waitForNavigation({ waitUntil: 'networkidle0' })
+  await page.waitForNavigation({
+    waitUntil: 'networkidle0'
+  })
 
   await Promise.all([
     page.click('#sidebar > div > div.subnav > ul > li:nth-child(2) > a'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.waitForNavigation({
+      waitUntil: 'networkidle0'
+    }),
   ])
 
   await Promise.all([
     page.click('#sidebar > div > ul:nth-child(2) > li > a'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.waitForNavigation({
+      waitUntil: 'networkidle0'
+    }),
   ])
 
   // Fill out Search for Sections
   await Promise.all([
     page.select('#VAR1', courseSemester),
     ...courses
-      .map((course, i) => {
-        const row = i + 1
-        const [courseSubject, courseCode] = course.split('*')
-        return [
-          page.select(`#LIST_VAR1_${row}`, courseSubject),
-          page.evaluate(
-            (courseCode, row) => {
-              const courseCodeInput = document.querySelector(`#LIST_VAR3_${row}`)
-              courseCodeInput.value = courseCode
-            },
-            courseCode,
-            row
-          ),
-        ]
-      })
-      .flat(),
+    .map((course, i) => {
+      const row = i + 1
+      const [courseSubject, courseCode] = course.split('*')
+      return [
+        page.select(`#LIST_VAR1_${row}`, courseSubject),
+        page.evaluate(
+          (courseCode, row) => {
+            const courseCodeInput = document.querySelector(`#LIST_VAR3_${row}`)
+            courseCodeInput.value = courseCode
+          },
+          courseCode,
+          row
+        ),
+      ]
+    })
+    .flat(),
   ])
 
   await Promise.all([
     page.click('#content > div.screen.WESTS12A > form > div > input'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    page.waitForNavigation({
+      waitUntil: 'networkidle0'
+    }),
   ])
 
   // Determine if offering is open - evaluated in browser
@@ -176,12 +219,20 @@ async function checkWebadvisor(courses) {
   return availableCourseInfo
 }
 
-async function sendEmail(courseMap, recipients) {  
+/**
+ * Emails all the users that have susbscribed to course information
+ * and havent been notified since the beginning of the day
+ * 
+ * @param {Object} courseMap - Parsed course data from webadvisor
+ * @param {Object} courseCodeEmailMap - Keys correspond to the course codes and the value 
+ * is an array of emails to send the course to
+ */
+async function sendEmail(courseMap, courseCodeEmailMap) {
   const EMAIL_SERVICE = process.env.EMAIL_SERVICE
   const EMAIL_ADDR = process.env.EMAIL_ADDR
   const EMAIL_PWD = process.env.EMAIL_PWD
 
-  if (!EMAIL_SERVICE || !EMAIL_ADDR || !EMAIL_PWD || !recipients) {
+  if (!EMAIL_SERVICE || !EMAIL_ADDR || !EMAIL_PWD) {
     console.warn('Email sending is not enabled. To enable email sending please set the EMAIL_SERVICE, EMAIL_ADDR and EMAIL_PWD env variables')
     return
   }
@@ -192,34 +243,109 @@ async function sendEmail(courseMap, recipients) {
       user: EMAIL_ADDR,
       pass: EMAIL_PWD
     }
-  });  
-  
-  let courseDetailsHTML = ''
-
-  Object.entries(courseMap).forEach(([courseCode, details]) => {
-    courseDetailsHTML += formatCourseInfoHTML(courseCode, details)
   })
 
-  if (!courseDetailsHTML) {
-    console.warn('No courses were available so no email was sent')
-    return
-  }
+  await Promise.all(Object.entries(courseCodeEmailMap).map(async ([courseCode, emails], i) => {
+    if (!emails || emails.length == 0 || !courseMap[courseCode] || courseMap[courseCode].length == 0) {
+      console.info(`no emails to send in batch ${i + 1} -- courses: ${courseMap[courseCode]} -- emails: ${emails}`)
+      return
+    }
 
-  const info = await transporter.sendMail({
-    from: EMAIL_ADDR, 
-    to: recipients, 
-    subject: 'Your course selection info', 
-    html: fs.readFileSync(__dirname + '/templates/coursesAvailableEmail.html', 'utf8').replace('${INSERT_INFO_HERE}', courseDetailsHTML),
-  });
+    const courseDetailsHTML = formatCourseInfoHTML(courseCode, courseMap[courseCode])
 
-  console.info('emails sent to', info.accepted)
-  console.info('emails did not send to', info.rejected)
+    const response = await transporter.sendMail({
+      from: EMAIL_ADDR,
+      to: EMAIL_ADDR,
+      bcc: emails,
+      subject: `Your course selection info for ${courseCode}`,
+      html: fs.readFileSync(__dirname + '/templates/coursesAvailableEmail.html', 'utf8').replace('${INSERT_INFO_HERE}', courseDetailsHTML),
+    })
+
+    // Filters out our email address from the response
+    response.accepted = response.accepted.filter(email => email != EMAIL_ADDR)
+    response.rejected = response.rejected.filter(email => email != EMAIL_ADDR)
+
+    console.warn(`batch ${i + 1} - failed emails`, response.rejected)
+    console.info(`batch ${i + 1} - successful emails`, response.accepted)
+
+    response.accepted.forEach((email) => {  
+      ddb.update({
+        TableName: 'Courses',
+        Key: { 'CourseCode': courseCode },
+        UpdateExpression: 'ADD #email :sentTimestamp',
+        ExpressionAttributeNames: {
+          '#email': email
+        },
+        ExpressionAttributeValues: {
+          ':sentTimestamp': ddb.createSet([new Date().setHours(0, 0, 0, 0)]),
+        }
+      }).promise().catch ((err) => {
+        console.error(`Error updating ${email} timestamp in dynamodb`, err)
+      })
+    })
+  }))
 }
 
 function formatCourseInfoHTML(courseCode, courseInfo) {
-  if (courseInfo.length) {
-    return `<p><b>${courseCode}</b> has sections available</p> <p>${courseInfo}</p> <br>`
+  if (!courseInfo.length) {
+    return ''
   }
 
-  return ''
+  return `<p><b>${courseCode}</b> has sections available</p> <p>${courseInfo}</p> <br>`
+}
+
+/**
+ * Queries dynamo db to find the user's to notify for each course
+ * 
+ * @param {Array} courseCodes - Array of course codes of available courses
+ * @returns {Object} - Keys correspond to the course codes and the value 
+ * is an array of emails to send the course to
+ */
+async function getEmailsCourseCodesFromDyanmo(courseCodes) {
+  let coursesQuery = []
+
+  // Formats all the course codes for DynamoDB
+  courseCodes.forEach(cc => coursesQuery.push({
+    'CourseCode': cc
+  }))
+
+  const CURRENT_DATE_UNIX = new Date().setHours(0, 0, 0, 0)
+  let courseCodeEmailMap = {}
+
+  try {
+    const responseData = await ddb.batchGet({
+      RequestItems: {
+        Courses: {
+          Keys: coursesQuery
+        }
+      }
+    }).promise()
+
+    responseData.Responses.Courses.forEach((courseData) => {
+      const courseCode = courseData.CourseCode
+      Object.entries(courseData).forEach(([key, val]) => {
+        if (!validator.isEmail(key)) {
+          // Filters out the DynamoDB metadata and non-email data (e.g the CourseCode)
+          console.info(`${key} is not an email`)
+          return
+        }
+
+        const email = key
+        const lastEmailTimestamp = val.values[val.values.length-1]
+
+        if (CURRENT_DATE_UNIX - lastEmailTimestamp >= UNIX_DAY) {
+          if (!courseCodeEmailMap[courseCode]) {
+            courseCodeEmailMap[courseCode] = []
+          }
+
+          // Assigns the course to to the user's email if they haven't gotten an email today
+          courseCodeEmailMap[courseCode].push(email)
+        }
+      })
+    })
+  } catch (err) {
+    console.error('error getting data from dynamodb', err)
+  }
+
+  return courseCodeEmailMap
 }
